e95713a6296059fe359fe1c264e50d6f04ed1aea
000Diggity000 <116099377+000Diggity000@users.noreply.github.com>
2023-01-16 08:25:45 +0100

    Create ModHelperData.cs

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "Laser Tower Mod";
+
+    public const string Description = "Adds a Laser Tower";
+
+    public const string Icon = "Icon.png";
+    public const string WorksOnVersion = "34.3";
+
+    public const string RepoOwner = "000Diggity000";
+    public const string RepoName = "LaserTower-BTD6";
+}
